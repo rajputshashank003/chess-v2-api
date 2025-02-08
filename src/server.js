0d6f8d6b28436c61df7b25dbd885d12de2cf8991
1rajputshashank@gmail.com <hariomshashank@gmail.com>
2025-02-08 16:32:28 +0530

    update mail send card

--- src/server.js
+++ src/server.js
@@ -3,14 +3,14 @@ dotenv.config();
 import { WebSocketServer } from 'ws';
 import { GameManager } from './GameManager.js';
 import {sendMail} from '../helper/mailer.js';
+import { msg } from "./utils/emailData.js";
 const wss = new WebSocketServer({ port: 8080 });
 
 const gameManager = new GameManager();
 
 wss.on('connection', function connection(ws) {
 
-  // const msg = `<p> Hello Sir ,\nNew user connected on <a href='https://chessv.netlify.app' >Chessv</a></p>`;
-  // sendMail(process.env.SEND_MAIL_TO, "ChessV user connected", msg);
+  sendMail(process.env.SEND_MAIL_TO, "ChessV user connected", msg);
   console.log("user connected");
   gameManager.addUser(ws);
 
